2718c6648d8ab4b45e0ed16f433e4dcdcda8eb4e
britneyhu <britneyhu2@gmail.com>
2025-12-16 13:09:23 -0800

    add favicon and logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
 
 import Button from "./Button";
 
 function Navbar({phase, navRoleVisible, handleNavRoleReveal, role, name}) {
     return (
         <nav className="w-full h-25 mb-10 p-5 bg-gray flex items-center justify-between">
-            <Link className="text-5xl cursor-pointer" to="/">M</Link>
+            <Link className="text-5xl cursor-pointer" to="/">
+                <img src={Logo} alt="Logo" className="h-20 w-20" />
+            </Link>
 
             <div className={(phase && phase !== "role") ? "flex items-center justify-center gap-5 p-5" : "hidden"}>
                 <div className="flex flex-col items-end justify-center">
