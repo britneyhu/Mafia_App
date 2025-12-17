50b19de5e0afbdde239334347e2e4608bff9954b
britneyhu <britneyhu2@gmail.com>
2025-12-17 10:19:42 -0800

    add package.json and dockerfile for deploy

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
     cors: { origin: "*" }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res)=>{
     res.send("Server is running");
